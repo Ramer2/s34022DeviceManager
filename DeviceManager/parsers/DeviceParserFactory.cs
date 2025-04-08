10fdea00b98b4de979db3528867758ff5e0d5166
s30422 <s30422@pjwstk.edu.pl>
2025-04-08 20:33:45 +0200

    DeviceParserFactory (additionally to previous commit)

---
+++ DeviceManager/parsers/DeviceParserFactory.cs
@@ -0,0 +1,17 @@
+﻿using DeviceManager.parsers;
+
+namespace DeviceManager.deviceManager;
+
+public static class DeviceParserFactory
+{
+    public static IDeviceParser? GetParser(string deviceId)
+    {
+        return deviceId.Split('-')[0] switch
+        {
+            "SW" => new SmartWatchParser(),
+            "P" => new PersonalComputerParser(),
+            "ED" => new EmbeddedDeviceParser(),
+            _ => null
+        };
+    }
+}
